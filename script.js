3cecba2161970a98fda1c71d1963464b4d21824f
ABODAHAB1 <mmwdy6168@gmail.com>
2025-12-10 23:09:55 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
-هلو
+// يخلي السهم ينزل لتحت عند الضغط
+document.querySelector('.scroll-down').addEventListener('click', () => {
+  window.scrollBy({
+    top: 400,
+    behavior: 'smooth'
+  });
+});
