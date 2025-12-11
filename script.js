57b4fa69985c7d4651a9652866129f033a711e9a
ABODAHAB1 <mmwdy6168@gmail.com>
2025-12-11 11:08:11 +0200

    Update script.js

--- script.js
+++ script.js
@@ -17,3 +17,26 @@ document.addEventListener("DOMContentLoaded", () => {
     info.hidden = !info.hidden; // toggle إظهار/إخفاء النبذة
   });
 });
+document.addEventListener("DOMContentLoaded", () => {
+  const arrow = document.querySelector('.scroll-down');
+  const services = document.getElementById('services');
+  const aboutSection = document.getElementById('about-section');
+  const contactSection = document.getElementById('contact-section');
+
+  arrow.addEventListener('click', () => {
+    services.classList.add('show');
+    aboutSection.classList.add('show');
+    contactSection.classList.add('show');
+    arrow.style.display = 'none';
+    services.scrollIntoView({ behavior: 'smooth' });
+  });
+
+  // زر نبذة عن صانع الموقع
+  const creatorBtn = document.getElementById('creator-btn');
+  const info = document.getElementById('creator-info');
+
+  creatorBtn.addEventListener('click', () => {
+    info.hidden = !info.hidden;
+  });
+});
+
